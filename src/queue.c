f1801c35aa69e7734edd4f689849e03f95397082
chill-cats <public.hoangnhanlenguyen@gmail.com>
2022-04-16 16:27:45 +0700

    Implement scheduler

--- src/queue.c
+++ src/queue.c
@@ -1,19 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "common.h"
 #include "queue.h"
 
-int empty(struct queue_t * q) {
-	return (q->size == 0);
+int empty(struct queue_t* q) { return (q->size == 0); }
+
+void enqueue(struct queue_t* q, struct pcb_t* proc) {
+    /* TODO: put a new process to queue [q] */
+    if (q->size == MAX_QUEUE_SIZE) {
+        return;  // no space left
+    }
+    q->proc[q->size] = proc;
+
+    ++q->size;
 }
 
-void enqueue(struct queue_t * q, struct pcb_t * proc) {
-	/* TODO: put a new process to queue [q] */	
-}
+struct pcb_t* dequeue(struct queue_t* q) {
+    /* TODO: return a pcb whose prioprity is the highest
+     * in the queue [q] and remember to remove it from q
+     * */
+    struct pcb_t* highestPriorityPCB      = NULL;
+    int           highestPriorityPCBIndex = -1;
 
-struct pcb_t * dequeue(struct queue_t * q) {
-	/* TODO: return a pcb whose prioprity is the highest
-	 * in the queue [q] and remember to remove it from q
-	 * */
-	return NULL;
-}
+    for (int index = 0; index < q->size; index++) {
+        if (highestPriorityPCB == NULL || highestPriorityPCB->priority < q->proc[index]->priority) {
+            highestPriorityPCB      = q->proc[index];
+            highestPriorityPCBIndex = index;
+        }
+    }
 
+    if (highestPriorityPCB != NULL) {
+        for (int index = highestPriorityPCBIndex; index < q->size - 1; index++) {
+            q->proc[index] = q->proc[index + 1];
+        }
+        --q->size;
+    }
+
+    return highestPriorityPCB;
+}
--- src/sched.c
+++ src/sched.c
@@ -1,42 +1,48 @@
-
+#include "common.h"
 #include "queue.h"
 #include "sched.h"
 #include <pthread.h>
 
-static struct queue_t ready_queue;
-static struct queue_t run_queue;
+#include "string.h"
+
+static struct queue_t  ready_queue;
+static struct queue_t  run_queue;
 static pthread_mutex_t queue_lock;
 
-int queue_empty(void) {
-	return (empty(&ready_queue) && empty(&run_queue));
-}
+int queue_empty(void) { return (empty(&ready_queue) && empty(&run_queue)); }
 
 void init_scheduler(void) {
-	ready_queue.size = 0;
-	run_queue.size = 0;
-	pthread_mutex_init(&queue_lock, NULL);
+    ready_queue.size = 0;
+    run_queue.size   = 0;
+    pthread_mutex_init(&queue_lock, NULL);
 }
 
-struct pcb_t * get_proc(void) {
-	struct pcb_t * proc = NULL;
-	/*TODO: get a process from [ready_queue]. If ready queue
-	 * is empty, push all processes in [run_queue] back to
-	 * [ready_queue] and return the highest priority one.
-	 * Remember to use lock to protect the queue.
-	 * */
-	return proc;
+struct pcb_t* get_proc(void) {
+    /*TODO: get a process from [ready_queue]. If ready queue
+     * is empty, push all processes in [run_queue] back to
+     * [ready_queue] and return the highest priority one.
+     * Remember to use lock to protect the queue.
+     * */
+    pthread_mutex_lock(&queue_lock);
+    if (empty(&ready_queue) && !empty(&run_queue)) {
+        memcpy(ready_queue.proc, run_queue.proc, run_queue.size * sizeof(struct pcb_t*));
+        memset(run_queue.proc, NULL, MAX_QUEUE_SIZE * sizeof(struct pcb_t*));
+        ready_queue.size = run_queue.size;
+        run_queue.size   = 0;
+    }
+    struct pcb_t* proc = dequeue(&ready_queue);
+    pthread_mutex_unlock(&queue_lock);
+    return proc;
 }
 
-void put_proc(struct pcb_t * proc) {
-	pthread_mutex_lock(&queue_lock);
-	enqueue(&run_queue, proc);
-	pthread_mutex_unlock(&queue_lock);
+void put_proc(struct pcb_t* proc) {
+    pthread_mutex_lock(&queue_lock);
+    enqueue(&run_queue, proc);
+    pthread_mutex_unlock(&queue_lock);
 }
 
-void add_proc(struct pcb_t * proc) {
-	pthread_mutex_lock(&queue_lock);
-	enqueue(&ready_queue, proc);
-	pthread_mutex_unlock(&queue_lock);	
+void add_proc(struct pcb_t* proc) {
+    pthread_mutex_lock(&queue_lock);
+    enqueue(&ready_queue, proc);
+    pthread_mutex_unlock(&queue_lock);
 }
-
-
