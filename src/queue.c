2e73f48c9a2acfff4185a9289dd403d0e4ddffcb
chill-cats <public.hoangnhanlenguyen@gmail.com>
2022-04-18 01:04:05 +0700

    Set new empty spot in ready queue to NULL

--- src/queue.c
+++ src/queue.c
@@ -1,30 +1,30 @@
+#include "queue.h"
+#include "common.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "common.h"
-#include "queue.h"
 
-int empty(struct queue_t* q) { return (q->size == 0); }
+int empty(struct queue_t *q) { return (q->size == 0); }
 
-void enqueue(struct queue_t* q, struct pcb_t* proc) {
+void enqueue(struct queue_t *q, struct pcb_t *proc) {
     /* TODO: put a new process to queue [q] */
     if (q->size == MAX_QUEUE_SIZE) {
-        return;  // no space left
+        return; // no space left
     }
     q->proc[q->size] = proc;
 
     ++q->size;
 }
 
-struct pcb_t* dequeue(struct queue_t* q) {
+struct pcb_t *dequeue(struct queue_t *q) {
     /* TODO: return a pcb whose prioprity is the highest
      * in the queue [q] and remember to remove it from q
      * */
-    struct pcb_t* highestPriorityPCB      = NULL;
-    int           highestPriorityPCBIndex = -1;
+    struct pcb_t *highestPriorityPCB = NULL;
+    int highestPriorityPCBIndex = -1;
 
     for (int index = 0; index < q->size; index++) {
         if (highestPriorityPCB == NULL || highestPriorityPCB->priority < q->proc[index]->priority) {
-            highestPriorityPCB      = q->proc[index];
+            highestPriorityPCB = q->proc[index];
             highestPriorityPCBIndex = index;
         }
     }
@@ -33,6 +33,7 @@ struct pcb_t* dequeue(struct queue_t* q) {
         for (int index = highestPriorityPCBIndex; index < q->size - 1; index++) {
             q->proc[index] = q->proc[index + 1];
         }
+        q->proc[q->size - 1] = NULL;
         --q->size;
     }
 
